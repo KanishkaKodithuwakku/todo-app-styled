3266a8abe166a53d7d507828f7772b8c960837fe
KanishkaKodithuwakku <infomail.kbk@gmail.com>
2023-09-01 02:47:26 +0530

    modified read me and styled

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,7 +2,13 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { StyledInput } from "../components/StyledInput";
 import { ButtonPrimary, Flex } from "../common/Buttons";
-
+const StyledFormWrapper = styled.div`
+  background-color: #f7f7f7;
+  padding: 15px;
+  margin-bottom: 15px;
+  border-radius: 4px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.05);
+`;
 const TodoForm = ({ addTodos }) => {
   const [todo, setTodo] = useState("");
 
@@ -11,19 +17,21 @@ const TodoForm = ({ addTodos }) => {
   };
 
   const handleAddTodo = (todo) => {
-      addTodos(todo)
-      setTodo('')
+    addTodos(todo);
+    setTodo("");
   };
 
   return (
-    <form>
-      <Flex>
-        <StyledInput type="text" value={todo} onChange={handleOnChange} />
-        <ButtonPrimary type="button" onClick={() => handleAddTodo(todo)}>
-          Add
-        </ButtonPrimary>
-      </Flex>
-    </form>
+    <StyledFormWrapper>
+      <form>
+        <Flex>
+          <StyledInput type="text" value={todo} onChange={handleOnChange} placeholder="Todo here..."/>
+          <ButtonPrimary type="button" onClick={() => handleAddTodo(todo)}>
+            Add
+          </ButtonPrimary>
+        </Flex>
+      </form>
+    </StyledFormWrapper>
   );
 };
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,34 @@
 import React from "react";
 import { StyledList, StyledListItem } from "./StyledLists";
-import { ButtonDanger} from "../common/Buttons";
+import { ButtonDanger } from "../common/Buttons";
+import { styled } from "styled-components";
+
+const StyledListWrapper = styled.div`
+  background-color: #fff;
+  padding: 15px;
+  border-radius: 4px;
+`;
 
 const TodoList = ({ todos, removeTodo }) => {
   const handleRemoveItems = (index) => {
     removeTodo(index);
   };
   return (
-    <StyledList>
-      {todos.map((todo, index) => (
-        <StyledListItem key={index}>
-          <span>{todo}</span>{" "}
-          <ButtonDanger type="button" onClick={() => handleRemoveItems(index)}>
-            Remove
-          </ButtonDanger>
-        </StyledListItem>
-      ))}
-    </StyledList>
+    <StyledListWrapper>
+      <StyledList>
+        {todos.map((todo, index) => (
+          <StyledListItem key={index}>
+            <span>{todo}</span>{" "}
+            <ButtonDanger
+              type="button"
+              onClick={() => handleRemoveItems(index)}
+            >
+              Remove
+            </ButtonDanger>
+          </StyledListItem>
+        ))}
+      </StyledList>
+    </StyledListWrapper>
   );
 };
 
